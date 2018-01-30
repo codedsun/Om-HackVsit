92b0958797ba59441f9ccb3359a413a3547b08ad
Suneet Srivastava <suneetbond91@gmail.com>
2018-01-30 11:35:39 +0530

    FirstStrt

---
+++ app/src/main/java/com/suneetsrivastava/om/Helpers/FontsListener.java
@@ -0,0 +1,26 @@
+package com.suneetsrivastava.om.Helpers;
+
+import android.content.Context;
+import android.graphics.Typeface;
+
+import com.suneetsrivastava.om.MyApplication;
+
+import java.lang.reflect.Type;
+
+/**
+ * Created by suneetsrivastava on 30/01/18.
+ */
+
+public class FontsListener {
+
+    Typeface typeface;
+    Context c;
+    String name;
+
+//    FontsListener(Context c,){
+//        typeface = Typeface.createFromAsset(c.getAssets());
+//    }
+//    public Typeface getFont(String fontName){
+
+    }
+}
