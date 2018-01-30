a0c4b3b724cf2e629fd3997015b2f5a3563aa686
Suneet Srivastava <suneetbond91@gmail.com>
2018-01-30 18:14:21 +0530

    SplashScreenAdded

---
+++ app/src/main/java/com/suneetsrivastava/om/Activities/LandingActivity.java
@@ -0,0 +1,15 @@
+package com.suneetsrivastava.om.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.suneetsrivastava.om.R;
+
+public class LandingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_landing);
+    }
+}
--- app/src/main/java/com/suneetsrivastava/om/Activities/MainActivity.java
+++ app/src/main/java/com/suneetsrivastava/om/Activities/MainActivity.java
@@ -2,14 +2,19 @@ package com.suneetsrivastava.om.Activities;
 
 import android.app.Activity;
 import android.app.Application;
+import android.content.Intent;
 import android.graphics.Typeface;
+import android.support.annotation.UiThread;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.View;
 import android.widget.ActionMenuView;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 import android.widget.Toolbar;
 
 
@@ -19,7 +24,7 @@ import com.suneetsrivastava.om.R;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements View.OnClickListener{
 
     FontsListener fontsListener;
     Typeface quoteView;
@@ -30,6 +35,20 @@ public class MainActivity extends Activity {
 
     @BindView(R.id.quote_text)
     TextView quoteText;
+
+    @BindView(R.id.editext_name)
+    EditText name;
+
+    @BindView(R.id.button_male)
+    Button male;
+
+    @BindView(R.id.button_female)
+    Button female;
+
+    @BindView(R.id.button_login)
+    Button login;
+
+    int gender = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,6 +65,9 @@ public class MainActivity extends Activity {
         quoteText.setTypeface(quoteView);
         appNameView = fontsListener.getFontByName("Satisfy-Regular");
         appName.setTypeface(appNameView);
+        male.setOnClickListener(this);
+        female.setOnClickListener(this);
+        login.setOnClickListener(this);
     }
 
     @Override
@@ -53,5 +75,25 @@ public class MainActivity extends Activity {
         MenuInflater menuInflater = getMenuInflater();
         menuInflater.inflate(R.menu.toolbar_menu,menu);
         return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v.getId() == male.getId()){
+            gender = 1;
+        }
+
+        if(v.getId() == female.getId()){
+            gender =2;
+        }
+
+        if(v.getId() == login.getId()){
+            if((name.getText().toString().isEmpty()) || (gender==0)  ){
+                Toast.makeText(this, "Enter The Details", Toast.LENGTH_SHORT).show();
+            }
+            else
+                startActivity(new Intent(this,LandingActivity.class));
+        }
+
     }
 }
